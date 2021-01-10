4c5a1e1faf34fc2169c89cc6f95c16c155998686
Anubhav Sarkar <anubhavsarkar7@gmail.com>
2021-01-10 16:46:30 +0530

    Removed About Me from header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,17 +6,12 @@ const Header = () => {
   return (
     <>
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-      <Navbar.Brand title="About Me" href="/">
-        Anubhav - Portfolio
-      </Navbar.Brand>
+        <Navbar.Brand title="About Me" href="/">
+          Anubhav - Portfolio
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Item title="About Me">
-              <Nav.Link className="nav-item" href="/">
-                About Me
-              </Nav.Link>
-            </Nav.Item>
             <Nav.Item title="Work Experience">
               <Nav.Link className="nav-item" href="/work-exp">
                 Work Experience
